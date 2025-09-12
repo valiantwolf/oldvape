82b6e46d354204945fb8d98541e05339458b03b1
valiantwolf <trariviste@gmail.com>
2025-09-12 21:00:57 +0200

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/VapeVoidware/VoidwareBakup/main/NewMainScript.lua", true))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/valiantwolf/oldvapebakup/main/NewMainScript.lua", true))()
